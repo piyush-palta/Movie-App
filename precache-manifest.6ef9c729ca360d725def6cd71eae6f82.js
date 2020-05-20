71490860a4a32b1f3350cabf0af538097c958b1d
piyush-palta <43702687+piyush-palta@users.noreply.github.com>
2020-05-20 23:13:14 +0530

    Updates

---
+++ precache-manifest.6ef9c729ca360d725def6cd71eae6f82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407ae22630bfb54925ab75dd860edea8",
+    "url": "/Movie-App/index.html"
+  },
+  {
+    "revision": "c56b9fa24777782e47b5",
+    "url": "/Movie-App/static/js/2.4d0194fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-App/static/js/2.4d0194fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b7880a97abc4230c6d",
+    "url": "/Movie-App/static/js/main.2bb5f974.chunk.js"
+  },
+  {
+    "revision": "71244337864b7094df30",
+    "url": "/Movie-App/static/js/runtime-main.9ff8c684.js"
+  }
+]);
